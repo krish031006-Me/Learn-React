1f8d81b885ed571186f3eb3834f75f2e3de80e55
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2025-11-12 21:31:08 +0530

    THis is the first commit for this project.

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,16 @@
+import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <HomePage />
-    </>
+  /* The routes component we have here tell the browser the pages we wanna add to our web app
+    the route component let's us add pages to out app*/
+  return ( // Route takes the path to the webpage and the HTML component we need to render also for path = / we can simply use word 'index'
+    <Routes>
+      <Route index element={ <HomePage /> }></Route>
+      <Route path='checkout' element={ <CheckoutPage /> } />
+    </Routes>
   )
 }
 
--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+// We are using a BrowserRouter component which is used to make the routing between pages a lot easier
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
