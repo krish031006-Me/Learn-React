e5920b1186703017a42d41f9839bb2e2ac70c37e
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2025-11-12 10:29:05 +0530

    plese commit it

--- chatbot-project/src/main.jsx
+++ chatbot-project/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// While rendering 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
