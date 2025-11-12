1e355fd01eaf32b9cfda6cf9b8661163744f50db
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2025-11-12 11:24:42 +0530

    Minor documentation changes

--- chatbot-project/src/main.jsx
+++ chatbot-project/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// While rendering 
+// While rendering we are wrapping the App component in StrictMode component which provides us additional checking for errors and all
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
